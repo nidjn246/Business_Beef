e909740ff10c2d99ad562b7fb2ec60186228ae7c
JerrelMartha <jerrelmarthatwo@gmail.com>
2025-11-19 09:47:47 +0100

    Pickup Systen

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using UnityEditor.ShaderKeywordFilter;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Pickup : MonoBehaviour
+{
+    [SerializeField] private Transform pickupPoint;
+    [SerializeField] private float pickupRange = 25f;
+    [SerializeField] private float throwForce = 5f;
+    [SerializeField] private LayerMask layer;
+    private GameObject heldObject;
+    [SerializeField] private bool isHolding = false;
+    private Vector2 gamepadDir;
+    private Vector3 aimDir;
+
+    private void Update()
+    {
+       
+        if (gamepadDir.sqrMagnitude > 0.1f)
+        {
+            aimDir = new Vector3(gamepadDir.x, 0, gamepadDir.y).normalized;
+        }
+    }
+
+
+    public void OnPickup(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        TryPickup();
+    }
+
+    public void OnDrop(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        DropProp();
+    }
+
+    public void OnThrow(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        ThrowProp();
+    }
+
+    public void OnAim(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        gamepadDir = context.ReadValue<Vector2>().normalized;
+        
+    }
+
+    public void OnAimMouse(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+
+        Vector2 mousePos = context.ReadValue<Vector2>();
+
+        Vector3 world = Camera.main.ScreenToWorldPoint(
+            new Vector3(mousePos.x, mousePos.y, Camera.main.nearClipPlane)
+        );
+
+        aimDir = (world - transform.position).normalized;
+        aimDir.z = 0;
+    }
+
+    private void TryPickup()
+    {
+        if (!isHolding)
+        {
+            Collider[] hits = Physics.OverlapSphere(pickupPoint.position, pickupRange, layer);
+
+            if (hits.Length == 0)
+            {
+                return;
+            }
+
+
+            GameObject obj = hits[0].gameObject;
+            heldObject = obj;
+
+            EquipProp(obj);
+        }
+    }
+
+    private void EquipProp(GameObject prop)
+    {
+        isHolding = true;
+        prop.transform.position = pickupPoint.position;
+        prop.transform.parent = pickupPoint;
+        prop.GetComponent<Rigidbody>().isKinematic = true;
+        prop.GetComponent<Collider>().enabled = false;
+    }
+
+    private void DropProp()
+    {
+        if (isHolding)
+        {
+            isHolding = false;
+            heldObject.transform.parent = null;
+            heldObject.GetComponent<Rigidbody>().isKinematic = false;
+            heldObject.GetComponent<Collider>().enabled = true;
+        }
+    }
+
+    private void ThrowProp()
+    {
+        if (!isHolding) return;
+
+        Rigidbody heldRb = heldObject.GetComponent<Rigidbody>();
+        Collider heldCollider = heldObject.GetComponent<Collider>();
+
+        isHolding = false;
+        heldObject.transform.parent = null;
+
+        heldRb.isKinematic = false;
+
+        // FIX: Re-enable collider when throwing
+        heldCollider.enabled = true;
+
+        Physics.IgnoreCollision(heldCollider, gameObject.GetComponent<Collider>());
+
+        heldRb.AddForce(aimDir * throwForce, ForceMode.Impulse);
+    }
+
+
+
+}
---
+++ Assets/ThrowableProp.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ThrowableProp", menuName = "Scriptable Objects/ThrowableProp")]
+public class ThrowableProp : ScriptableObject
+{
+    [SerializeField] private float damage;
+    
+}
