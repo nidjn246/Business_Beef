44bbc5ff62cf7e17d3a25752247556d1d9d88e13
JerrelMartha <jerrelmarthatwo@gmail.com>
2025-12-01 15:36:46 +0100

    .

--- Assets/Scripts/Ladder.cs
+++ Assets/Scripts/Ladder.cs
@@ -1,84 +1,14 @@
 ﻿using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Ladder : MonoBehaviour
 {
-    private bool playerInside = false;
-    private GameObject player;
-    private float offset = -1f;
+    public Transform upTransform;
+    public Transform downTransform;
 
-
-    private void OnTriggerEnter(Collider other)
+    public void EnterLadderState(Transform player)
     {
-        if (other.CompareTag("Player"))
-        {
-            playerInside = true;
-            player = other.gameObject;
-        }
-    }
+        bool enteredFromBelow = player.position.y < transform.position.y;
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            playerInside = false;
-
-            // re-enable gravity
-            other.GetComponent<Rigidbody>().useGravity = true;
-
-        }
-    }
-
-    public void EnterLadder(InputAction.CallbackContext ctx)
-    {
-
-        // If player is inside ladder trigger
-        if (playerInside && ctx.performed)
-        {
-            // If ALREADY climbing → exit ladder
-            if (player.GetComponent<PlayerMovement>().isClimbingLadder)
-            {
-                ExitLadder();
-            }
-            else
-            {
-                // Not climbing → enter ladder
-                EnterLadderState();
-            }
-        }
-    }
-
-    private void EnterLadderState()
-    {
-        if (!player.GetComponent<PlayerMovement>().isClimbingLadder) return;
-        Debug.Log("Enter ladder");
-
-        PlayerState.currentState = PlayerState.playerState.OnLadder;
-
-        // disable gravity while climbing
-        player.GetComponent<Rigidbody>().useGravity = false;
-
-        player.GetComponent<PlayerMovement>().isClimbingLadder = true;
-
-        // snap player onto ladder
-        player.transform.position = new Vector3(
-            transform.position.x,
-            player.transform.position.y,
-            transform.position.z + offset
-        );
-    }
-
-    private void ExitLadder()
-    {
-        Debug.Log("Exit ladder");
-        PlayerState.currentState = PlayerState.playerState.InControl;
-
-        player.GetComponent<PlayerMovement>().isClimbingLadder = false;
-
-        // restore gravity
-        player.GetComponent<Rigidbody>().useGravity = true;
-
-        // move player back to original position
-        player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, 0f);
+        player.position = enteredFromBelow ? upTransform.position : downTransform.position;
     }
 }
