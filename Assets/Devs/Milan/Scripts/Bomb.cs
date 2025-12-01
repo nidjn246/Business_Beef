6780eff4954105226fc8ff7fd57ffb7e7453b6f9
Milan Veldhuizen <milanveldhuizen09@gmail.com>
2025-12-01 10:37:11 +0100

    bomberman

---
+++ Assets/Devs/Milan/Scripts/Bomb.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : ThrowableProp
+{
+    [SerializeField] private float explosionForce = 5f;
+    [SerializeField] private List<GameObject> playersInRange;
+    public override void Die()
+    {
+        for (int i = 0; i < playersInRange.Count; i++)
+        {
+            playersInRange[i].GetComponent<Rigidbody>().AddExplosionForce(explosionForce, transform.position, 5f, 1f, ForceMode.Impulse);
+        }
+        base.Die();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && playersInRange.Contains(other.gameObject) == false)
+            playersInRange.Add(other.gameObject);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            playersInRange.Remove(other.gameObject);
+    }
+
+}
