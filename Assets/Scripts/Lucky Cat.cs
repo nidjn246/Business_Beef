8ff30bc007a51c53c1348d7074c4c9a5f500ceff
JerrelMartha <jerrelmarthatwo@gmail.com>
2025-12-02 15:29:15 +0100

    lucky cat & bomb fixed

--- Assets/Scripts/Lucky Cat.cs
+++ Assets/Scripts/Lucky Cat.cs
@@ -1,8 +1,13 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
 public class LuckyCat : ThrowableProp
 {
     override public void Die()
     {
         AudioManager.PlaySound(SoundType.CATMEOW, true, 0.3f);
         base.Die();
+
+        
     }
 }
