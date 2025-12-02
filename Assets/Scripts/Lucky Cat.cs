b04d05c25cdc169c1d16d8b16b6f53308570dabe
JerrelMartha <jerrelmarthatwo@gmail.com>
2025-12-02 15:42:29 +0100

    Sounds

--- Assets/Scripts/Lucky Cat.cs
+++ Assets/Scripts/Lucky Cat.cs
@@ -5,7 +5,7 @@ public class LuckyCat : ThrowableProp
 {
     override public void Die()
     {
-        AudioManager.PlaySound(SoundType.CATMEOW, true, 0.3f);
+        AudioManager.PlaySound(SoundType.CATMEOW, true, 0.6f);
         base.Die();
 
         
